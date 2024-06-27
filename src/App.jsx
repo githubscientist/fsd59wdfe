a528bbb964e1f4f9d8fe05008fd4ead8f3b75a58
githubscientist <sathish.datascientist@gmail.com>
2024-06-27 13:11:10 +0530

    bootstrap setup done + react router dom setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+]);
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
